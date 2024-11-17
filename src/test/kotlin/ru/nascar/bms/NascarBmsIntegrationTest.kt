7cc7f2633a4de05933f8798760856eacb7822498
Karim Khasan <karimhasan3879@gmail.com>
2024-11-17 19:03:46 +0300

    issue-25: basic test examples

--- src/test/kotlin/ru/nascar/bms/NascarBmsIntegrationTest.kt
+++ src/test/kotlin/ru/nascar/bms/NascarBmsIntegrationTest.kt
@@ -3,6 +3,7 @@ package ru.nascar.bms
 import com.github.springtestdbunit.DbUnitTestExecutionListener
 import com.github.springtestdbunit.annotation.DatabaseOperation
 import com.github.springtestdbunit.annotation.DatabaseTearDown
+import net.devh.boot.grpc.client.inject.GrpcClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -13,9 +14,16 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
+import ru.nascar.bms.presentation.abstractions.BarServiceGrpc
 import java.time.Clock
 
-@SpringBootTest
+@SpringBootTest(
+    properties = [
+        "grpc.server.inProcessName=test",
+        "grpc.server.port=9091",
+        "grpc.client.barService.address=in-process:test"
+    ]
+)
 @Transactional(propagation = Propagation.SUPPORTS)
 @TestExecutionListeners(
     value = [
@@ -35,4 +43,10 @@ import java.time.Clock
 class NascarBmsIntegrationTest {
     @Autowired
     protected lateinit var clock: Clock
+    @GrpcClient("barService")
+    protected lateinit var barServiceGrpc: BarServiceGrpc.BarServiceBlockingStub
+
+    companion object {
+        const val USER_ID = "testUser"
+    }
 }
