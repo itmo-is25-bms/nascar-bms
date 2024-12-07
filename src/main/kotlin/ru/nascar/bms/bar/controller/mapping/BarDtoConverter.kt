ca217b1ec00b32775791e6b4a260aecfa14a67e9
DmitryBulin <dmitrybulindev@gmail.com>
2024-12-07 12:52:24 +0300

    issue-46: return summary

--- src/main/kotlin/ru/nascar/bms/bar/controller/mapping/BarDtoConverter.kt
+++ src/main/kotlin/ru/nascar/bms/bar/controller/mapping/BarDtoConverter.kt
@@ -1,10 +1,30 @@
 package ru.nascar.bms.bar.controller.mapping
 
 import ru.nascar.bms.bar.domain.model.Bar
-import ru.nascar.bms.presentation.abstractions.BarServiceProto.BarDto
+import ru.nascar.bms.presentation.abstractions.BarServiceProto.*
 
-fun Bar.toDto(): BarDto = BarDto.newBuilder()
-    .setId(id)
-    .setName(name)
-    .setAddress(address)
-    .build()
+fun Bar.toDto(): BarDto {
+    val barDtoBuilder = BarDto.newBuilder()
+
+    barDtoBuilder
+        .setId(id)
+        .setName(name)
+        .setAddress(address)
+
+    val barSummaryDtoBuilder = BarSummaryDto.newBuilder()
+
+    if (summaryTags != null) {
+        barSummaryDtoBuilder
+            .addAllTags(summaryTags)
+    }
+
+    if (score != null) {
+        barSummaryDtoBuilder
+            .setScore(score)
+    }
+
+    barDtoBuilder
+        .setBarSummary(barSummaryDtoBuilder.build())
+
+    return barDtoBuilder.build()
+}
